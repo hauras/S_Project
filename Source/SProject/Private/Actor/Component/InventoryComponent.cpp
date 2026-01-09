be5ae69b8ed056d0c1ea14af3ff87aa68b404842
haura <92637707+hauras@users.noreply.github.com>
2026-01-09 10:03:42 +0900

    [fix] 스킬 ui 쿨타임 오류 수정

--- Source/SProject/Private/Actor/Component/InventoryComponent.cpp
+++ Source/SProject/Private/Actor/Component/InventoryComponent.cpp
@@ -38,22 +38,19 @@ void UInventoryComponent::UseItem(UItemDataAsset* ItemData)
 				FGameplayEffectContextHandle Context = ASC->MakeEffectContext();
 				FGameplayEffectSpecHandle Spec = ASC->MakeOutgoingSpec(ItemData->ItemEffectClass, 1.f, Context);
         
-				// 영구 지속(Infinite) 이펙트를 적용하고 영수증(Handle)을 받습니다.
 				FActiveGameplayEffectHandle NewHandle = ASC->ApplyGameplayEffectSpecToSelf(*Spec.Data.Get());
         
-				// 나중에 무기 벗을 때 쓰려고 영수증을 저장해둡니다.
 				EquipmentEffectHandles.Add(Slot, NewHandle);
 			}
 			
 			if (EquippedItems.Contains(Slot))
 			{
-				Inventory.Add(EquippedItems[Slot]); // 옛날 템 가방행
+				Inventory.Add(EquippedItems[Slot]); 
 			}
 
 			// 새 템 장착
 			EquippedItems.Add(Slot, ItemData);
             
-			// 장착 UI에게 알림
 			OnEquipmentChanged.Broadcast(Slot, ItemData);
 		}
 		// [2] 소모품(포션 등)인 경우
